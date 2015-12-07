b24550ab7f225e3d2030ceae0ec7ee740a5e3f18
Elana Kopelevich <ekopelevich@gmail.com>
2015-12-07 15:54:53 -0700

    server3.js working

--- server3.js
+++ server3.js
@@ -1,3 +1,10 @@
+// Create a Node HTTP server that serves up the requested file
+// The server accepts 2 query parameters: from and to
+// Before sending out the file, change all instance of from in the file to to
+// Example: http://localhost:1337/index.html?from=hello&to=bye will change all instances of hello in index.html to bye
+
+//This one works!!
+
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
@@ -6,7 +13,7 @@ var path = require('path');
 var app = http.createServer( function( request, response) {
   var parsedUrl = url.parse( request.url, true);
   console.log( parsedUrl );
-  fs.readFile( path.join ('.', parsedUrl.pathname), 'utf8', function( err, contents ) {
+  fs.readFile( path.join('.', parsedUrl.pathname), 'utf8', function( err, contents ) {
     if ( err ) {
       response.statusCode = 404;
       response.end ( '404' );
